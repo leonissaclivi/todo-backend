fd62c200a0fe65c9388d876a65bb4119d58a3e00
Leon <leonissaclivi@gmail.com>
2025-04-09 00:18:19 +0530

    corrected res.cookie

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,21 +3,25 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv').config();
 
-const userCreate = async (req,res,next)=>{
+const userCreate = async (req, res, next) => {
     try {
-        const {email, password} = req.body;
-    const hashedPassword = bcrypt.hashSync(password, 10);
+        const { email, password } = req.body;
+        const hashedPassword = bcrypt.hashSync(password, 10);
 
-    const newUser = new User({
-        email, password:hashedPassword
-    });
+        const newUser = new User({
+            email, password: hashedPassword
+        });
 
-    await newUser.save();
+        await newUser.save();
 
-    const token = jwt.sign({data: email}, process.env.secret_key);
-    res.cookie('token', token)
+        const token = jwt.sign({ data: email }, process.env.secret_key);
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'strict'
+        });
 
-    res.json({message:'User succesfully registered.'})
+        res.json({ message: 'User succesfully registered.' })
 
 
     } catch (error) {
@@ -25,23 +29,23 @@ const userCreate = async (req,res,next)=>{
     }
 }
 
-const userLogin = async (req,res,next) =>{
+const userLogin = async (req, res, next) => {
     try {
-        const {email, password} = req.body;
-    const doesUserExist = await User.findOne({email});
-    if (!doesUserExist){
-        return res.status(404).json({message:'User not found'});
-    }
-    const passwordMatch = bcrypt.compareSync(password, doesUserExist.password);
-    if(!passwordMatch){
-        return res.status(400).json({ message: 'Invalid login credentials' });
-    }
-    const token = jwt.sign({ data:email}, process.env.secret_key);
-    res.cookie('token', token)
-    res.json({message:'User login successfull'})
+        const { email, password } = req.body;
+        const doesUserExist = await User.findOne({ email });
+        if (!doesUserExist) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        const passwordMatch = bcrypt.compareSync(password, doesUserExist.password);
+        if (!passwordMatch) {
+            return res.status(400).json({ message: 'Invalid login credentials' });
+        }
+        const token = jwt.sign({ data: email }, process.env.secret_key);
+        res.cookie('token', token)
+        res.json({ message: 'User login successfull' })
     } catch (error) {
         next(error);
     }
 }
 
-module.exports = {userCreate, userLogin}
+module.exports = { userCreate, userLogin }
