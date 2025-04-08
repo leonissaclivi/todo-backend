a600fda2f3e2c87ac07e11c572bc8899cf5a6071
Leon <leonissaclivi@gmail.com>
2025-04-09 01:45:14 +0530

    added user id to todo model

--- models/todoModel.js
+++ models/todoModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const todoSchema = new mongoose.Schema({
     task:{type:String, required:true},
     completed:{type:Boolean, default:false},
-    user:{type: mongoose.Schema.Types.ObjectId, ref:"User"}
+    userId: { type: mongoose.Schema.Types.ObjectId, required: true }
 })
 
 module.exports = mongoose.model('todos', todoSchema)
