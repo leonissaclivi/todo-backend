75736d01d43e0b8709d1718ca4b8a40e6b92da59
Leon <leonissaclivi@gmail.com>
2025-04-18 23:54:16 +0530

    added patch method cors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 const corsOptions = {
     origin: 'http://localhost:5173',
     credentials: true, 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'] 
+    methods: ['GET', 'POST', 'PUT','PATCH', 'DELETE'] 
   };
 app.use(cors(corsOptions));
 
