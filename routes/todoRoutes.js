2ff0eb41121c63a8dea3d4749686ae5bbdc87990
Leon <leonissaclivi@gmail.com>
2025-04-18 23:14:25 +0530

    delete route added

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const todos = require('../models/todoModel');
 
-const { getUserTodos, createTodo, updateTodo } = require('../controllers/todoController');
+const { getUserTodos, createTodo, updateTodo, deleteTodo } = require('../controllers/todoController');
 
 const authMiddleware = require('./middleware');
 
@@ -11,5 +11,7 @@ router.get('/',authMiddleware, getUserTodos);
 router.post('/',authMiddleware, createTodo);
 
 router.patch('/:id',authMiddleware, updateTodo);
+
+router.delete('/:id',authMiddleware, deleteTodo);
 
 module.exports = router;
